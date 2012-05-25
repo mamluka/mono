f0ddb196de434072bac6c6250a1c59eefbe09ecc
Iain Ballard <iain.ballard@7digital.com>
2012-05-25 17:26:57 +0100

    not working yet :-(

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,30 +1,29 @@
-script "install_something" do
+script "install_mono" do
   interpreter "bash"
   code <<-EOH
-	echo "MONO: installing dependencies"
-	sudo apt-get install g++
-	sudo apt-get install bison
-	sudo apt-get install gettext
-	sudo apt-get install make
-	sudo apt-get install pkg-config
-	sudo apt-get install libglib2.0-dev
+echo "MONO: installing dependencies"
+sudo apt-get install g++
+sudo apt-get install bison
+sudo apt-get install gettext
+sudo apt-get install make
+sudo apt-get install pkg-config
+sudo apt-get install libglib2.0-dev
 
-	echo "MONO: Getting mono from source"
-	wget 'http://download.mono-project.com/sources/mono/mono-2.11.1.tar.bz2'
+echo "MONO: Getting mono from source"
+wget 'http://download.mono-project.com/sources/mono/mono-2.11.1.tar.bz2'
 
-	tar -xvjf 'mono-2.11.1.tar.bz2'
+tar -xvjf 'mono-2.11.1.tar.bz2'
 
-	cd mono-2.11.1
+cd mono-2.11.1
 
-	echo "MONO: Configuring and install mono"
-	echo
-	./configure
+echo "MONO: Configuring and install mono"
+echo
+./configure
 
-	make
+make
 
-	sudo make install
+sudo make install
 
-	echo "MONO: Done"
-	echo
+echo "MONO: Done"
   EOH
 end
