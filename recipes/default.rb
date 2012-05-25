f0c250c10878d056c464abd39fc6612d07544ba8
Iain Ballard <iain.ballard@7digital.com>
2012-05-25 15:59:51 +0100

    recipe

---
+++ recipes/default.rb
@@ -0,0 +1,30 @@
+script "install_something" do
+  interpreter "bash"
+  code <<-EOH
+	echo "MONO: installing dependencies"
+	sudo apt-get install g++
+	sudo apt-get install bison
+	sudo apt-get install gettext
+	sudo apt-get install make
+	sudo apt-get install pkg-config
+	sudo apt-get install libglib2.0-dev
+
+	echo "MONO: Getting mono from source"
+	wget 'http://download.mono-project.com/sources/mono/mono-2.11.1.tar.bz2'
+
+	tar -xvjf 'mono-2.11.1.tar.bz2'
+
+	cd mono-2.11.1
+
+	echo "MONO: Configuring and install mono"
+	echo
+	./configure
+
+	make
+
+	sudo make install
+
+	echo "MONO: Done"
+	echo
+  EOH
+end
