81f4ecd006873bdcddfd0035993407da5216fa83
Iain Ballard <iain.ballard@7digital.com>
2012-06-06 16:56:10 +0100

    apt-get for mono

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,28 +2,9 @@ script "install_mono" do
   interpreter "bash"
   code <<-EOH
 echo "MONO: installing dependencies"
-sudo apt-get install g++
-sudo apt-get install bison
-sudo apt-get install gettext
-sudo apt-get install make
-sudo apt-get install pkg-config
-sudo apt-get install libglib2.0-dev
-
-echo "MONO: Getting mono from source"
-wget 'http://download.mono-project.com/sources/mono/mono-2.11.1.tar.bz2'
-
-tar -xvjf 'mono-2.11.1.tar.bz2'
-
-cd mono-2.11.1
-
-echo "MONO: Configuring and install mono"
-echo
-./configure
-
-make
-
-sudo make install
-
+sudo apt-get install mono-complete
+sudo apt-get install mono-devel
+sudo apt-get install mono-xbuild
 echo "MONO: Done"
   EOH
 end
