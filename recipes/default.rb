f10d3739b15f175c48c2e5c800d96da7d7ae8006
Iain Ballard <iain.ballard@7digital.com>
2012-06-07 10:33:23 +0100

    Auto accept apt-gets

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,12 @@
 script "install_mono" do
   interpreter "bash"
   code <<-EOH
+echo "Mono install started" >> /monoinst.log
 echo "MONO: installing dependencies"
-sudo apt-get install mono-complete
-sudo apt-get install mono-devel
-sudo apt-get install mono-xbuild
+sudo apt-get install mono-complete -y
+sudo apt-get install mono-devel -y
+sudo apt-get install mono-xbuild -y
 echo "MONO: Done"
+echo "Mono install completed" >> /monoinst.log
   EOH
 end
