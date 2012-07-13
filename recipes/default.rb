72701fec862e15ecce664a771ec108abab6f7d37
Iain Ballard <iain.ballard@7digital.com>
2012-07-13 12:29:33 +0100

    different version

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,13 +9,14 @@ sudo apt-get install pkg-config -y
 sudo apt-get install libglib2.0-dev -y
 
 
-wget 'http://download.mono-project.com/sources/mono/mono-2.11.2.tar.bz2'
+wget 'http://download.mono-project.com/sources/mono/mono-2.10.8.tar.bz2'
   	
-tar -xvjf 'mono-2.11.2.tar.bz2' 	
-cd mono-2.11.2
+tar -xvjf 'mono-2.10.8.tar.bz2' 	
+cd mono-2.10.8
 
 
 ./configure  	
+#make get-monolite-latest
 make
 sudo make install
   EOH
